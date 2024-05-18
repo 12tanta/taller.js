3171cb0fcf0702cb5416b0dec134f861fe438181
12tanta <rojastantaai.23@gmail.com>
2024-05-18 16:25:00 -0500

    Add files via upload

---
+++ practica.js
@@ -0,0 +1,44 @@
+let numerodeestudiantes = 5;
+console.log(numerodeestudiantes);
+numerodeestudiantes = numerodeestudiantes-1;
+console.log(numerodeestudiantes);
+numerodeestudiantes --;
+console.log(numerodeestudiantes);
+// para incremetar el numero de estudiantes (asistentecia)
+// hay dos secciones a y b con 45 alumnos
+
+let alumnostotal =45;
+let seccionA = 25 -alumnostotal;
+console.log('tonces en seccion b seria', seccionA);
+
+alumnostotal += 5
+console.log(alumnostotal);
+
+// REDUCIR UN NUMERO
+alumnostotal -=2
+console.log(alumnostotal);
+
+// secuencia de escape
+
+let rojas=45
+console.log('son valores',rojas);
+/*
+\'  comillas simple
+\" comilla doble
+\\ barra invertida 
+\n linea nueva
+\r retorno de carro
+\t tabulacion
+\b retroceso
+\f salto de pagina 
+*/
+
+// concatenar las variables 
+
+let datos = " rojas" + " tanta"
+console.log( datos);
+    
+
+
+
+
